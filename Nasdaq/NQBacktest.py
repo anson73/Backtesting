9187de070fb3a650bc115e54624f92cf801bb53d
AnsonL <ansonl112233@gmail.com>
2025-08-11 20:32:27 +1000

    Added starter files and data

---
+++ Nasdaq/NQBacktest.py
@@ -0,0 +1,12 @@
+
+def findHigh():
+    # Find which day is most likely to create the high of the week
+    return
+
+def findLow():
+    # Find which day is most likely to create the low of the week
+    return
+
+
+if __name__ == "__main__":
+    print("")
